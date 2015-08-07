44143b5a0645ca93212aca618bc7a34a54b2ad48
Travis Crampton <crampton.travis@gmail.com>
2015-08-07 12:05:52 -0700

    keeps track of how many times a word is listed int he array

--- lib/word_repeat.rb
+++ lib/word_repeat.rb
@@ -1,25 +1,18 @@
 class String
   define_method(:word_repeat) do
-    self.split()
-    sentence = self.split()
-    sentence
+    words = self.split()
+    words
     counts = Hash.new 0
-      sentence.each do |word|
-        counts[sentence] += 1
+      words.each do |word|
+        counts[word] += 1
       end
+      counts
     end
   end
 
-
-
-
-
-
-
-#
-# words = ["travis", "loves", "loves", "programming"]
-#
-# counts = Hash.new 0
-#   words.each do |word|
-#     counts[word] += 1
-#   end
+  #  words = ["travis", "loves", "loves", "programming"]
+  #
+  #  counts = Hash.new 0
+  #    words.each do |word|
+  #      counts[word] += 1
+  #    end
--- spec/word_repeat_spec.rb
+++ spec/word_repeat_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('word_repeat')
 
   describe('String#word_repeat') do
-    it('converts string into an array') do
-      expect(("travis loves loves programming").word_repeat()).to(eq(["travis", "loves", "loves", "programming"]))
+  #   it('converts string into an array') do
+  #     expect(("travis loves loves programming").word_repeat()).to(eq(["travis", "loves", "loves", "programming"]))
+  # end
+    it('keeps track of how many times a word is listen in the array') do
+      expect(("travis loves loves programming").word_repeat()).to(eq({"travis"=>1, "loves"=>2, "programming"=>1}))
+    end
   end
-end
