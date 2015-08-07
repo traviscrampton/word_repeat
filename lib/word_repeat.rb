7ed53254e6b9162c08e004c2bfca9152e0603bed
Travis Crampton <crampton.travis@gmail.com>
2015-08-07 11:16:10 -0700

    change string into array fix

--- lib/word_repeat.rb
+++ lib/word_repeat.rb
@@ -1,6 +1,25 @@
 class String
-  define_method(:word_repeat) do |find|
-    separate = self.split()
-    separate.include?(find)
+  define_method(:word_repeat) do
+    self.split()
+    sentence = self.split()
+    sentence
+    counts = Hash.new 0
+      sentence.each do |word|
+        counts[sentence] += 1
+      end
+    end
   end
-end
+
+
+
+
+
+
+
+#
+# words = ["travis", "loves", "loves", "programming"]
+#
+# counts = Hash.new 0
+#   words.each do |word|
+#     counts[word] += 1
+#   end
--- spec/word_repeat_spec.rb
+++ spec/word_repeat_spec.rb
@@ -2,10 +2,7 @@ require('rspec')
 require('word_repeat')
 
   describe('String#word_repeat') do
-  #  it('separates a sentence into an array') do
-  #    expect(('travis loves programming').word_repeat).to(eq(["travis", "loves", "programming"]))
-  #  end
-    it('takes an argument and sees if its included is included in the array') do
-      expect(("travis loves programming").word_repeat("loves")).to(eq(true))
+    it('converts string into an array') do
+      expect(("travis loves loves programming").word_repeat()).to(eq(["travis", "loves", "loves", "programming"]))
   end
 end
